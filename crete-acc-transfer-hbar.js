d75b75d7b4f842d9bdfb4e40af3242994abd1d0e
Sahril Nanda Putra <55041897+sahrilputra@users.noreply.github.com>
2023-04-12 22:32:51 +0700

    create the Hedera testnet client

--- crete-acc-transfer-hbar.js
+++ crete-acc-transfer-hbar.js
@@ -3,12 +3,25 @@ const { Client } = require("@hashgraph/sdk")
 require("dotenv").config();
 
 const createAccTransferHbar = async () => {
-    const accountId = process.env.MY_PRIVATE_KEY ;
+    const accountId = process.env.MY_PRIVATE_KEY;
     const privateKey = process.env.MY_ACCOUNT_ID;
 
-    if(accountId == null || privateKey == null){
+    if (accountId == null || privateKey == null) {
         throw new Error("Account ID or PRIVATE key is missing");
     }
+
+    // Part 2 - Create connection to the Hedera network
+    const client = Client.forTestnet();
+    client.setOperator(accountId, privateKey);
+    // Part 3 - Create new account
+
+    // Part 4 - Get the new account's ID
+
+    // Part 5 - Verify the new account's starting balance
+
+    // Part 6 - Transfer 100 TINYBARs from Hedera Testnet account to new account
+
+    // Part 7 - Verify the new account's current balance
 }
 
 createAccTransferHbar();
