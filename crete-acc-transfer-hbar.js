87c492eda5b31e4b2f37dfe4934dba8ec8e4671d
Sahril Nanda Putra <55041897+sahrilputra@users.noreply.github.com>
2023-04-12 22:56:33 +0700

    add Transfer To Testnet

--- crete-acc-transfer-hbar.js
+++ crete-acc-transfer-hbar.js
@@ -1,37 +1,48 @@
-const { Client } = require("@hashgraph/sdk")
+const { Client, PrivateKey, AccountCreateTransaction, AccountBalanceQuery, Hbar, TransferTransaction } = require("@hashgraph/sdk");
 
 require("dotenv").config();
 
 const createAccTransferHbar = async () => {
-    const accountId = process.env.MY_PRIVATE_KEY;
-    const privateKey = process.env.MY_ACCOUNT_ID;
+    const myAccountId = process.env.MY_ACCOUNT_ID;
+    const myPrivateKey = process.env.MY_PRIVATE_KEY;
 
-    if (accountId == null || privateKey == null) {
+    if (myAccountId == null || myPrivateKey == null) {
         throw new Error("Account ID or PRIVATE key is missing");
     }
 
     // Part 2 - Create connection to the Hedera network
     const client = Client.forTestnet();
-    client.setOperator(accountId, privateKey);
+    client.setOperator(myAccountId, myPrivateKey);
 
     // Part 3 - Create new account
-    const newAccountPrivateKey = privateKey.generateED25519();
-    const newAccountPublicKey = newAccountPrivateKey.publicKey();
+    const newAccountPrivateKey = PrivateKey.generateED25519();
+    const newAccountPublicKey = newAccountPrivateKey.publicKey;
     const newAccount = await new AccountCreateTransaction()
         .setKey(newAccountPublicKey)
-        .setInitialBalance(Hbar.fromTinyBars(1000))
+        .setInitialBalance(Hbar.fromTinybars(1000))
         .execute(client);
-    
+
     // Part 4 - Get the new account's ID
     const getReceipt = await newAccount.getReceipt(client);
     const newAccountId = getReceipt.accountId;
-    console.log("New Account ID is : " + newAccount );
+    console.log("The new account ID is: " + newAccountId);
 
     // Part 5 - Verify the new account's starting balance
+    const accountBalance = await new AccountBalanceQuery().setAccountId(newAccountId).execute(client);
+    console.log("The new account starting balance is: " + accountBalance.hbars.toTinybars() + " TINYBARs");
 
     // Part 6 - Transfer 100 TINYBARs from Hedera Testnet account to new account
+    const sendHbar = await new TransferTransaction()
+        .addHbarTransfer(myAccountId, Hbar.fromTinybars(-100)) //Sending
+        .addHbarTransfer(newAccountId, Hbar.fromTinybars(100)) //Recieve
+        .execute(client);
+
+    const transactionReceipt = await sendHbar.getReceipt(client);
+    console.log("Status Of Transfer Transsaction : " + transactionReceipt.status.toString());
 
     // Part 7 - Verify the new account's current balance
+    const newAccountBalance = await new AccountBalanceQuery().setAccountId(newAccountId).execute(client);
+    console.log("The new account current balance is: " + newAccountBalance.hbars.toTinybars() + " TINYBARs");
 }
 
 createAccTransferHbar();
