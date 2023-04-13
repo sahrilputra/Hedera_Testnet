6a1e620ff2ef13bec5fb53b5ef13a42f9e7b0999
Sahril Nanda Putra <55041897+sahrilputra@users.noreply.github.com>
2023-04-13 20:24:31 +0700

    create Topic Transaction

--- non-fungible-token.js
+++ non-fungible-token.js
@@ -3,12 +3,15 @@ const {
     AccountId,
     PrivateKey,
     Client,
-    TokenSupplyType,
-    TokenType,
     TokenCreateTransaction,
+    TokenType,
+    TokenSupplyType,
     TokenMintTransaction,
-
+    TransferTransaction,
+    AccountBalanceQuery,
+    TokenAssociateTransaction,
 } = require("@hashgraph/sdk");
+
 
 const aliceId = AccountId.fromString(process.env.ALICE_ACCOUNT_ID);
 const aliceKey = PrivateKey.fromString(process.env.ALICE_PRIVATE_KEY);
@@ -50,10 +53,28 @@ async function createNonFungibleToken() {
     console.log(`- Created NFT ${tokenId} with serial: ${mintRx.serials[0].low} \n`);
 
     // Part 3 - Associate token to user account
-
+    let associateAliceTx = await new TokenAssociateTransaction().setAccountId(bobId).setTokenIds([tokenId]).freezeWith(client).sign(bobKey);
+    let associateAliceTxSubmit = await associateAliceTx.execute(client);
+    let associateAliceRx = await associateAliceTxSubmit.getReceipt(client);
+    console.log(`- NFT association with Bob's account: ${associateAliceRx.status}\n`);
 
     // Part 4 - Transfer token
+    // balance check (before)
+    var balanceCheckTx = await new AccountBalanceQuery().setAccountId(aliceId).execute(client);
+    console.log(`- Alice balance: ${balanceCheckTx.tokens._map.get(tokenId.toString())} NFTs of ID ${tokenId}`);
+    var balanceCheckTx = await new AccountBalanceQuery().setAccountId(bobId).execute(client);
+    console.log(`- Bob's balance: ${balanceCheckTx.tokens._map.get(tokenId.toString())} NFTs of ID ${tokenId}`);
 
+    let tokenTransferTx = await new TransferTransaction().addNftTransfer(tokenId, 1, aliceId, bobId).freezeWith(client).sign(aliceKey);
+    let tokenTransferSubmit = await tokenTransferTx.execute(client);
+    let tokenTransferRx = await tokenTransferSubmit.getReceipt(client);
+    console.log(`\n- NFT transfer from Treasury to Alice: ${tokenTransferRx.status} \n`);
+
+    // balance check (after)
+    var balanceCheckTx = await new AccountBalanceQuery().setAccountId(aliceId).execute(client);
+    console.log(`- Alice balance: ${balanceCheckTx.tokens._map.get(tokenId.toString())} NFTs of ID ${tokenId}`);
+    var balanceCheckTx = await new AccountBalanceQuery().setAccountId(bobId).execute(client);
+    console.log(`- Bob's balance: ${balanceCheckTx.tokens._map.get(tokenId.toString())} NFTs of ID ${tokenId}`);
 
 }
 
